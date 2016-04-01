7b324de3f5e398f1f78df1b7a2691a6f2a863ece
unknown <zephor@qq.com>
2016-04-01 11:29:34 +0800

    add check open before send to use

--- pooled_pika.py
+++ pooled_pika.py
@@ -11,7 +11,7 @@ from twisted.internet.protocol import ClientCreator
 
 __all__ = ["VERSION", "PooledConn"]
 
-VERSION = "0.1.4"
+VERSION = "0.1.5"
 
 logger = logging.getLogger(__name__)
 
@@ -166,11 +166,12 @@ class PooledConn(object):
         if channel:
             d.addCallback(self._get_channel)
         with _lock():
-            if self.__idle_pool:
+            while self.__idle_pool:
                 _id, conn = self.__idle_pool.popitem()
-                self.__using_pool[_id] = conn
-                d.callback(conn)
-                return d
+                if conn.is_open():
+                    self.__using_pool[_id] = conn
+                    d.callback(conn)
+                    return d
         if self.size >= self.__max_size:
             self.waiting.append(d)
         else:
